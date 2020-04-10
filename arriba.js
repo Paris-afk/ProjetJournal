f9e2f026bcc96843763da7868be5038671605b77
Paris Lopez <paris.obed@hotmail.com>
2020-04-10 16:14:44 +0200

    best button

--- arriba.js
+++ arriba.js
@@ -2,8 +2,17 @@ $(document).ready(function(){
 
     $('.ir-arriba').click(function(){
         $('body,html').animate({
-            scrollTop:0
-        } ,150);
-    })
+            scrollTop: '0px'
+        } ,300);
+    });
+
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>100){
+            $('.ir-arriba').slideDown(300);
+        }else{
+            $('.ir-arriba').slideUp(300);
+        }
+    });
 
 });
